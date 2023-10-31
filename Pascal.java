55c3abe0d6575a1139dfbb4087c354a292079567
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-10-31 18:02:49 +0530

    pascal Triangle

--- Pascal.java
+++ Pascal.java
@@ -1,18 +1,21 @@
-import java.lang.Math;
 class Pascal{
-public static void main(String args[]){
-	for(int i=1;i<=5;i++){
-		for(int j=1;j<=5;j++){
-			if(j<=5-i){
+	static int fact(int n){
+		if(n==0 || n==1)
+			return 1;
+		else
+			return n*fact(n-1);
+	}
+	public static void main(String args[]){
+		int i,j,n=5,res=0;
+		for(i=0;i<n;i++){
+			for(j=0;j<n-i;j++){
 				System.out.print(" ");
 			}
-			else{
-				System.out.print(i+" ");
-				
-			}
+					for(j=0;j<=i;j++){
+						res=fact(i)/(fact(i-j)*fact(j));
+						System.out.print(res+" ");
+					}
+			System.out.println();
 		}
-		System.out.print("\n");
 	}
-	
-}	
 }
