4e05870abc7b4989d0010cc5165920319e04a304
MadhuMohanS <madhumohan.xworkz@gmail.com>
2023-10-31 12:26:04 +0530

    pascal Triangle

---
+++ Pascal.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+class Pascal{
+public static void main(String args[]){
+	for(int i=1;i<=5;i++){
+		for(int j=1;j<=5;j++){
+			if(j<=5-i){
+				System.out.print(" ");
+			}
+			else{
+				System.out.print(i+" ");
+				
+			}
+		}
+		System.out.print("\n");
+	}
+	
+}	
+}
